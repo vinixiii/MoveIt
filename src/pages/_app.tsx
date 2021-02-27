9394c066ec03e18f451b569db650b68abd0ed5fb
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-02-27 14:55:33 -0300

    ChallengesContext criado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import '../styles/global.css'
 
+import { ChallengesProvider } from '../contexts/ChallengesContext'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    //Todos os elementos dentro do Provider terá acesso aos dados desse contexto
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  );  
 }
 
 export default MyApp
